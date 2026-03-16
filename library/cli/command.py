1ad8b44505af4a69facf5814facdcc919750958f
Simon Alligand <armadamaindefeer@gmail.com>
2026-03-16 22:34:41 +0100

    refactor: moved help command to App class and deprecated autocompletion func

--- library/cli/command.py
+++ library/cli/command.py
@@ -3,7 +3,9 @@ import library.cmdUtils.cmdUtils as cutils
 from library.common import info, warn, error, SOURCE, getSource
 import sys
 import typing
+from warnings import deprecated
 
+@deprecated("Buggy function, will be removed")
 def getAvailableToken(values:list[str],buffer:str):
 	token_set = {token[:len(buffer)+1] for token in values if token.startswith(buffer)}
 	if len(token_set) > 1 or len(token_set) == 0:
@@ -16,6 +18,7 @@ def getAvailableToken(values:list[str],buffer:str):
 		else:
 			return getAvailableToken(values,returnToken)
 
+@deprecated("Buggy function, will be removed")
 def autoComplete(char_buffer:str,values:list[str],alwaysShow=False) ->str:
 	if not alwaysShow:
 		display_token = [token for token in values if token.startswith(char_buffer)]
@@ -29,6 +32,7 @@ def autoComplete(char_buffer:str,values:list[str],alwaysShow=False) ->str:
 		print()
 	return getAvailableToken(values,char_buffer)
 
+@deprecated("Buggy function, will be removed")
 def autoCompleteFromToken(tokens:list[str],completionList:list[str],alwaysShow=False) -> str:
 	if len(tokens) == 3 or (len(tokens) == 2 and tokens[1].count(" ") > 0):
 		if len(tokens) == 2:
@@ -42,11 +46,13 @@ def autoCompleteFromToken(tokens:list[str],completionList:list[str],alwaysShow=F
 	else:
 		return "".join(tokens)
 
+@deprecated("Buggy function, will be removed")
 def autoCompleteFromList(completionList:list[str],alwaysShow=False) -> typing.Callable:
 	def wrapped_autoComplete(tokens:list[str]):
 		return autoCompleteFromToken(tokens,completionList,alwaysShow)
 	return wrapped_autoComplete
 
+@deprecated("Buggy function, will be removed")
 def autoCompleteFromGen(completionGenFunc:typing.Callable,alwaysShow=False) -> typing.Callable:
 	def wrapped_autoComplete(tokens:list[str]):
 		return autoCompleteFromToken(tokens,completionGenFunc(),alwaysShow)
@@ -57,23 +63,4 @@ def _exit(input:Shell) -> None:
 	exitCode:str = input[0] if len(input.input) > 0 else ""
 	if cutils.Validate("Voulez vous quitter ?",SOURCE,enterIsYes=True): #TEXT
 		sys.exit(exitCode)
-
-@Register("help","help (TOPIC ENTRY)","Show help", optional=1,autoComplete=autoCompleteFromList(["Command"])) #TEXT
-def _help(input:Shell) -> None:
-	if len(input) == 0:
-		for command in globalCommands:
-			info("- " + command.syntax)
-			info("\t" + command.desc)
-			info("\tusage : "+ command.usage)
-
-	if len(input) == 1:
-		for command in globalCommands:
-			if input[0] == command.syntax:
-				info("- " + command.syntax)
-				info("\t" + command.desc)
-				info("\tusage : "+ command.usage)
-				break
-		else:
-			error(f"Unrecognised commande : '{input[0]}'")
-			return
 
